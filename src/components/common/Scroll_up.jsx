770603ec995f0c28124524fa8891f0a9dd801dc3
kimsangjunv1 <ingyoe64@gmail.com>
2023-08-22 20:19:40 +0900

    Fix: 스크롤이 최상단에 있을경우 Top 안보이게 숨김

--- src/components/common/Scroll_up.jsx
+++ src/components/common/Scroll_up.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Scroll_up = () => {
-  return <div className="up_btn">Scroll_up</div>;
+  const scrollBtn = document.querySelector(".up_btn");
+  const scrollBtnDisplay = function () {
+    window.scrollY > window.innerHeight
+      ? scrollBtn.classList.add("show")
+      : scrollBtn.classList.remove("show");
+  };
+
+  window.addEventListener("scroll", scrollBtnDisplay);
+  const scrollWindow = () => {
+    window.scroll({
+      top: 0,
+      left: 100,
+      behavior: "smooth",
+    });
+    // if (window.scrollY != 0) {
+    //   setTimeout(function () {
+    //     window.scrollTo(0, window.scrollY - 50);
+    //     scrollWindow();
+    //   }, 10);
+    // }
+  };
+  return (
+    <div
+      className="up_btn"
+      onClick={() => {
+        scrollWindow();
+      }}
+    >
+      Scroll_up
+    </div>
+  );
 };
 
 export default Scroll_up;
