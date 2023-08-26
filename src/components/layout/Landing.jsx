0b80180cac903ad6fb2dcc7f562415c617f3037d
kimsangjunv1 <ingyoe64@gmail.com>
2023-08-26 17:44:51 +0900

    Improve: 마우스 드래그시 따라오도록 설정

--- src/components/layout/Landing.jsx
+++ src/components/layout/Landing.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+import { useEffect } from "react";
+
 import Landing_glow from "./../../assets/img/landing_bg_glow.svg";
 
 import Landing_fur_001 from "./../../assets/img/landing_fur_001.svg";
@@ -9,16 +12,72 @@ import Landing_fur_005 from "./../../assets/img/landing_fur_005.svg";
 import Landing_fur_006 from "./../../assets/img/landing_fur_006.svg";
 
 const Landing = () => {
+  useEffect(() => {
+    let rotate_que = Math.floor(Math.random() * 101);
+    // let size_que = Math.floor(Math.random()*5)+1;
+    const allList = document.querySelectorAll(".fur_container img");
+
+    document.querySelector(".landing").addEventListener("mousemove", (e) => {
+      //마우스 좌표 값
+      let mousePageX = e.pageX;
+      let mousePageY = e.pageY;
+
+      // 전체 가로
+      let centerPageX = window.innerWidth / 2 - mousePageX;
+      let centerPageY = window.innerHeight / 2 - mousePageY;
+
+      //이미지 움직이기
+      for (let i = 1; i <= allList.length; i++) {
+        for (let q = 1; q <= 6; q++) {
+          document.querySelector(
+            ".fur_container img:nth-child(" + i + ")"
+          ).style.transform =
+            "translate(" +
+            -centerPageX / (i * 5) +
+            "px, " +
+            -centerPageY / (i * 5) +
+            "px) rotate(" +
+            centerPageX * (i * 0.01) +
+            "deg)";
+        }
+      }
+    });
+  }, []);
+
   return (
     <div className="landing">
       <div className="landing_inner">
         <div className="fur_container">
-          <img src={Landing_fur_001} alt="솜뭉치 캐릭터 001" />
-          <img src={Landing_fur_002} alt="솜뭉치 캐릭터 001" />
-          <img src={Landing_fur_003} alt="솜뭉치 캐릭터 001" />
-          <img src={Landing_fur_004} alt="솜뭉치 캐릭터 001" />
-          <img src={Landing_fur_005} alt="솜뭉치 캐릭터 001" />
-          <img src={Landing_fur_006} alt="솜뭉치 캐릭터 001" />
+          <img
+            className="fur_001"
+            src={Landing_fur_001}
+            alt="솜뭉치 캐릭터 001"
+          />
+          <img
+            className="fur_002"
+            src={Landing_fur_002}
+            alt="솜뭉치 캐릭터 001"
+          />
+          <img
+            className="fur_003"
+            src={Landing_fur_003}
+            alt="솜뭉치 캐릭터 001"
+          />
+          <img
+            className="fur_004"
+            src={Landing_fur_004}
+            alt="솜뭉치 캐릭터 001"
+          />
+          <img
+            className="fur_005"
+            src={Landing_fur_005}
+            alt="솜뭉치 캐릭터 001"
+          />
+          <img
+            className="fur_006"
+            src={Landing_fur_006}
+            alt="솜뭉치 캐릭터 001"
+          />
         </div>
         <h2 className="main_text">
           <em>즐기며</em> 개발하는
