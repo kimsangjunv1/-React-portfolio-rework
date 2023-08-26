a14c5f286747e91aec1dc034ea0a4c4e66761431
kimsangjunv1 <ingyoe64@gmail.com>
2023-08-26 16:55:36 +0900

    Improve: 솜뭉치 이미지 추가

--- src/components/layout/Landing.jsx
+++ src/components/layout/Landing.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Landing_glow from "./../../assets/img/landing_bg_glow.svg";
 
+import Landing_fur_001 from "./../../assets/img/landing_fur_001.svg";
+import Landing_fur_002 from "./../../assets/img/landing_fur_002.svg";
+import Landing_fur_003 from "./../../assets/img/landing_fur_003.svg";
+import Landing_fur_004 from "./../../assets/img/landing_fur_004.svg";
+import Landing_fur_005 from "./../../assets/img/landing_fur_005.svg";
+import Landing_fur_006 from "./../../assets/img/landing_fur_006.svg";
+
 const Landing = () => {
   return (
     <div className="landing">
       <div className="landing_inner">
+        <div className="fur_container">
+          <img src={Landing_fur_001} alt="솜뭉치 캐릭터 001" />
+          <img src={Landing_fur_002} alt="솜뭉치 캐릭터 001" />
+          <img src={Landing_fur_003} alt="솜뭉치 캐릭터 001" />
+          <img src={Landing_fur_004} alt="솜뭉치 캐릭터 001" />
+          <img src={Landing_fur_005} alt="솜뭉치 캐릭터 001" />
+          <img src={Landing_fur_006} alt="솜뭉치 캐릭터 001" />
+        </div>
         <h2 className="main_text">
           <em>즐기며</em> 개발하는
           <br />
