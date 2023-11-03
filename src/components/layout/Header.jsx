8886c8a24a1089e0056322c2d9c3bd26b851b6bc
kimsangjunv1 <ingyoe64@gmail.com>
2023-11-03 15:16:13 +0900

    Fix: json 수정

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,67 +7,69 @@ import LogoBackground from "./../../assets/img/logo_bg.jpg";
 import LogoMain from "./../../assets/img/logo.svg";
 
 const Header = ({ scrolled }) => {
-  const headerBox = useRef(null);
+	const headerBox = useRef(null);
 
-  const [nowScroll, setNowScroll] = useState(true);
+	const [nowScroll, setNowScroll] = useState(true);
 
-  const [lastScroll, setLastScroll] = useState(0);
-  const [offsetTop, setOffsetTop] = useState(headerBox.current?.offsetTop);
-  const [menuItem, setMenuItem] = useState(null);
+	const [lastScroll, setLastScroll] = useState(0);
+	const [offsetTop, setOffsetTop] = useState(headerBox.current?.offsetTop);
+	const [menuItem, setMenuItem] = useState(null);
 
-  useEffect(() => {
-    document.querySelector(".scroll_progress").style.width = scrolled + "%";
+	useEffect(() => {
+		document.querySelector(".scroll_progress").style.width = scrolled + "%";
 
-    const checkTop = () => {
-      if (offsetTop >= window.scrollY) {
-        // 안닿았을때
-      } else {
-        // 닿았을때
-        let scrollTop =
-          window.pageYOffset ||
-          window.scrollY ||
-          document.documentElement.scrollTop;
+		const checkTop = () => {
+			if (offsetTop >= window.scrollY) {
+				// 안닿았을때
+			} else {
+				// 닿았을때
+				let scrollTop =
+					window.pageYOffset ||
+					window.scrollY ||
+					document.documentElement.scrollTop;
 
-        // console.log(scrollTop < lastScroll);
+				// console.log(scrollTop < lastScroll);
 
-        if (scrollTop < lastScroll) {
-          //현재 스크롤 값이 이전 스크롤 값보다 작다면
-          headerBox.current.style.top = "0px";
-        } else {
-          headerBox.current.style.top = "-85px";
-        }
-        setLastScroll(scrollTop); // 지금 현재 스크롤 값을 이전 스크롤 값에 넣은 뒤
-      }
-    };
+				if (scrollTop < lastScroll) {
+					//현재 스크롤 값이 이전 스크롤 값보다 작다면
+					headerBox.current.style.top = "0px";
+				} else {
+					headerBox.current.style.top = "-85px";
+				}
+				setLastScroll(scrollTop); // 지금 현재 스크롤 값을 이전 스크롤 값에 넣은 뒤
+			}
+		};
 
-    window.addEventListener("scroll", checkTop);
-  });
+		window.addEventListener("scroll", checkTop);
+	});
 
-  useEffect(() => {
-    setOffsetTop(headerBox.current?.offsetTop);
-  }, []);
+	useEffect(() => {
+		setOffsetTop(headerBox.current?.offsetTop);
+	}, []);
 
-  return (
-    <header ref={headerBox}>
-      <div className="header_inner">
-        <HeaderMenu title={"Information"} />
-        <HeaderMenu title={"Technics"} />
-        <HeaderMenu title={"Study"} />
-        <HeaderMenu title={"Careers"} />
-        <a className="logo" href="#">
-          <img className="bg" src={LogoBackground} alt="로고_뒷배경" />
-          <img className="icon" src={LogoMain} alt="로고_메인" />
-        </a>
-        <HeaderMenu title={"Projects"} />
-        <HeaderMenu title={"What"} />
-        <HeaderMenu title={"Whatever"} />
-        <HeaderMenu title={"Shooting"} />
-      </div>
-      <div className="scroll_background">
-        <div className="scroll_progress"></div>
-      </div>
-    </header>
-  );
+	return (
+		<header ref={headerBox}>
+			<div className="header_inner">
+				{/* <HeaderMenu title={"Information"} /> */}
+				{/* <HeaderMenu title={"Technics"} /> */}
+				<HeaderMenu title={"Home"} />
+				<HeaderMenu title={"Information"} />
+				<HeaderMenu title={"Skill"} />
+				<a className="logo" href="#">
+					<img className="bg" src={LogoBackground} alt="로고_뒷배경" />
+					<img className="icon" src={LogoMain} alt="로고_메인" />
+				</a>
+				<HeaderMenu title={"Project"} />
+				<HeaderMenu title={"Study"} />
+				<HeaderMenu title={"Contact"} />
+				{/* <HeaderMenu title={"Whatever"} />
+        <HeaderMenu title={"Shooting"} /> */}
+			</div>
+			<div className="scroll_background">
+				<div className="scroll_progress"></div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
